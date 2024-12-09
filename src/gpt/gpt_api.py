a23fb27e98630bcb026465eae89be5ebb3eae03f
Falah Rasyidi <s4007133@student.rmit.edu.au>
2024-12-09 18:19:01 +1100

    Able to retrieve up to 9 articles

--- src/gpt/gpt_api.py
+++ src/gpt/gpt_api.py
@@ -4,6 +4,9 @@ from duckduckgo_search import DDGS
 from python.rake.rake import *
 from newspaper import Article
 
+import requests
+import json
+
 app = FastAPI()
 
 # Create a request model for incoming prompts
@@ -28,17 +31,82 @@ async def extract(request: RequestModel):
 
 @app.post("/retrieve")
 async def retrieve(request: RequestModel):
+    articles: list = []
+
     # Fetch articles from The Guardian
-    url = "https://www.thescottishsun.co.uk/sport/13976409/john-higgins-snooker-soap-dallas-specialist-subject-mastermind/"
+    endpoint = "https://content.guardianapis.com/search?q=football&api-key=f76306d2-0a71-41c7-9271-1d2a405b8b61"
+    response = requests.get(endpoint)
 
-    article = Article(url, language="en")
-    article.download()
-    article.parse()
+    if response.status_code == 200:
+        data = response.json()
 
-    print("ARTICLE TEXT")
-    print(f"==={article.text}===")
+        # Only retrieve the first three articles
+        for i in range(0, 3):
+            article = { 'url': "", 'title': "", 'pubdate': "", 'text': "" }
 
-    return {"message": article.text}
+            # Download and parse article
+            article_parsed = Article(data['response']['results'][i]['webUrl'], language="en")
+            article_parsed.download()
+            article_parsed.parse()
+
+            # Store article information in a dictionary
+            article.update({
+                'url': data['response']['results'][i]['webUrl'],
+                'title': article_parsed.title,
+                'pubdate': data['response']['results'][i]['webPublicationDate'],
+                'text': article_parsed.text
+            })
+
+            # Push to articles list
+            articles.append(article)
+    
+    # Fetch articles from News API
+    endpoint = "https://newsapi.org/v2/everything?q=tesla&from=2024-11-09&sortBy=publishedAt&apiKey=e6782b4eb6ed4ec6a943526bcc54b8e6"
+    response = requests.get(endpoint)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        for i in range(0, 3):
+            article = { 'url': "", 'title': "", 'pubdate': "", 'text': "" }
+
+            article_parsed = Article(data['articles'][i]['url'], language="en")
+            article_parsed.download()
+            article_parsed.parse()
+
+            article.update({
+                'url': data['articles'][i]['url'],
+                'title': article_parsed.title,
+                'pubdate': data['articles'][i]['publishedAt'],
+                'text': article_parsed.text
+            })
+
+            articles.append(article)
+    
+    # Fetch articles from News Data
+    endpoint = "https://newsdata.io/api/1/latest?apikey=pub_61660406963f4c81935fba712030890a7e333&q=football&country=au,us&language=en"
+    response = requests.get(endpoint)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        for i in range(0, 3):
+            article = { 'url': "", 'title': "", 'pubdate': "", 'text': "" }
+
+            article_parsed = Article(data['results'][i]['link'], language="en")
+            article_parsed.download()
+            article_parsed.parse()
+
+            article.update({
+                'url': data['results'][i]['link'],
+                'title': article_parsed.title,
+                'pubdate': data['results'][i]['pubDate'],
+                'text': article_parsed.text
+            })
+
+            articles.append(article)
+
+    return articles
 
 @app.post("/chat")
 async def chat(request: RequestModel):
